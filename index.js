3091eeb436d5fd2c38848801c8df719f4b463222
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-19 23:34:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(musicians, instruments){
-  var temp = {};
+  var temp = [];
   for ( let i=0; i < musicians.length; i++){
     temp.push('$musicians[i] plays $instruments[i]');
   }
